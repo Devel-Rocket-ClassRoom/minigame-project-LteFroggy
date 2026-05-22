e4554e88b925c0b605595d5308a68c404778892f
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-22 10:11:28 +0900

    fix : 빌드 시 저장이 되지 않던 문제 수정

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -4,6 +4,7 @@ public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T> {
 	private static T _instance;
 	public static T Instance {
 		get {
+			if (_instance != null) return _instance; 
 			_instance = FindAnyObjectByType<T>();
 			if (_instance == null) { _instance = new GameObject(typeof(T).Name).AddComponent<T>(); }
 			return _instance;
