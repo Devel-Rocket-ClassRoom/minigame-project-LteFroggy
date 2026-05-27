9ac851c157bf4c7a26e4dc90489aba86ed97889a
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-27 15:11:33 +0900

    feat(#77) : StartScene 및 StartNodeManager 추가
    - Start 노드에 진입했을 때 보여줄 씬과 다음 노드로 진행하는 버튼 핸들러가
      필요해 StartScene/StartNodeManager 추가
    - 게임 진입 직후 UIScene 부트스트래핑/씬 전환을 트리거할 런처로 MainScene 추가

---
+++ Assets/Scripts/UI/Start/StartNodeManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartNodeManager : MonoBehaviour {
+	[Header("=== 다음 노드로 갈 버튼 ===")]
+	[SerializeField] private Button _button;
+
+	// 버튼 누르면 다음 노드로 가게 함.
+	private void OnEnable() {
+		_button.onClick.AddListener(GameEvents.NodeCompleted);
+	}
+
+	private void OnDisable() {
+		_button.onClick.RemoveListener(GameEvents.NodeCompleted);
+	}
+}
