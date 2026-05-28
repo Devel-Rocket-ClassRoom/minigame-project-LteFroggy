15f2fb88e1111e004e30077e5591fe73890df4b1
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-05-28 17:14:55 +0900

    fix(#67) : BurnCardAction Execute에서 릴릭/버프 보정값 반영

--- Assets/Scripts/Action/CardActions/BurnCardAction.cs
+++ Assets/Scripts/Action/CardActions/BurnCardAction.cs
@@ -1,5 +1,19 @@
-﻿namespace Action.CardActions {
-	public class BurnCardAction {
+﻿public class BurnCardAction : CardAction {
+	public int amount;
+	protected override int Amount => amount;
+	protected override int CalculateAmountWithContext(CardUseContext context) {
+		int result = amount;
+		result = context.user.CalculateGiveBurn(result);
+		result = context.relicManager.CalculateAmountWithRelics(context.cardInfo, this, result);
 		
+		return result;
 	}
+	
+	public override void Execute(CardUseContext context) {
+		var burn = new Burn();
+		burn.Init(context.target, CalculateAmountWithContext(context), 0);
+		context.target.AddStatus(burn);
+	}
+	
+	public override string CardDescriptionKey => "BurnCardText";
 }
