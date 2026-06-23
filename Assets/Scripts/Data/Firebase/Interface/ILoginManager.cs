670d5b09c00b410796eee302a060f2a778ec0f18
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-06-23 15:34:55 +0900

    feat(#255) : 메인 메뉴 로그인 패널 연결
    Firebase 사용 시 새 런 진입 전에 로그인 패널을 거치도록 메인 메뉴 흐름을 연결합니다.
    
    게스트 버튼으로 익명 로그인을 호출하고, 로그인 완료 후 로드아웃으로 이어지도록 씬 참조를 구성합니다.

---
+++ Assets/Scripts/Data/Firebase/Interface/ILoginManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public interface ILoginManager {
+	public event Action<string, string> OnEmailSignInClicked;
+	public event Action<string, string> OnEmailSignUpClicked;
+	public event Action OnAnonymousSignInClicked;
+
+	public void ShowInfo(string message);
+	public void ShowError(string message);
+	public void ClearStatus();
+}
--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,22 +9,91 @@ public class MainMenuManager : MonoBehaviour {
 	[SerializeField] private Button _newRunButton;
 	[SerializeField] private Button _quitButton;
 	[SerializeField] private GameObject _loadoutPanel;
+	[SerializeField] private LoginManager _loginPanel;
 
-	private void OnEnable() {
-		_newRunButton.onClick.AddListener(OpenLoadout);
+	private async void OnEnable() {
+		_newRunButton.interactable = false;
 		_quitButton.onClick.AddListener(QuitGame);
+
+		PrepareNewRunButtonAsync().Forget();
 	}
 
 	private void OnDisable() {
-		_newRunButton.onClick.RemoveListener(OpenLoadout);
+		_newRunButton.onClick.RemoveAllListeners();
 		_quitButton.onClick.RemoveListener(QuitGame);
 	}
+	
+	private async UniTaskVoid PrepareNewRunButtonAsync() {
+		await UniTask.WaitUntil(() => FirebaseBootstrapper.Instance != null);
 
+		FirebaseBootstrapper bootstrapper = FirebaseBootstrapper.Instance;
+		InitState state = await bootstrapper.WaitForInitializationAsync();
+
+		_newRunButton.onClick.RemoveAllListeners();
+
+		if (state == InitState.Ready) {
+			_newRunButton.onClick.AddListener(OpenLogin);
+		} else {
+			_newRunButton.onClick.AddListener(OpenLoadout);
+		}
+
+		_newRunButton.interactable = true;
+	}
+	
 	private void OpenLoadout() {
+		CloseLoginPanel();
 		_loadoutPanel.SetActive(true);
 		if (_loadoutPanel.TryGetComponent(out LoadoutManager loadoutManager))
 			loadoutManager.RefreshRelicList();
 	}
+	
+	private void OpenLogin() {
+		// 열릴 때, 이미 로그인 되어이쓴 상태라면 패널 열 필요 없이 바로 로드아웃으로
+		if (FirebaseBootstrapper.Instance.AuthManager.IsLoggedIn) {
+			Debug.Log("[MainMenuManager] 이미 로그인되어있어 바로 로드아웃 패널로 연결됩니다.");
+			OpenLoadout();
+			return;
+		}
+		
+		_loadoutPanel.SetActive(false);
+		if (_loginPanel == null) {
+			Debug.LogError("[MainMenuManager] 로그인 패널이 연결되어 있지 않습니다.");
+			return;
+		}
+		
+		_loginPanel.OnAnonymousSignInClicked += OnAnonymousSignInClicked;
+		_loginPanel.OnEmailSignInClicked += OnEmailSignInClicked;
+		_loginPanel.OnEmailSignUpClicked += OnEmailSignUpClicked;
+		
+		_loginPanel.gameObject.SetActive(true);
+	}
+	
+	private async void OnAnonymousSignInClicked() {
+		var (result, error) = await FirebaseBootstrapper.Instance.AuthManager.SignIn();
+		if (result) { OpenLoadout(); }
+		else { _loginPanel.ShowError(error); }
+	}
+	
+	private async void OnEmailSignUpClicked(string email, string password) {
+		var (result, error) = await FirebaseBootstrapper.Instance.AuthManager.SignUp(email, password);
+		if (result) { OpenLoadout(); }
+		else { _loginPanel.ShowError(error); }
+	}
+	
+	private async void OnEmailSignInClicked(string email, string password) {
+		var (result, error) = await FirebaseBootstrapper.Instance.AuthManager.SignIn(email, password);
+		if (result) { OpenLoadout(); }
+		else { _loginPanel.ShowError(error); }
+	}
+	
+	private void CloseLoginPanel() {
+		_loginPanel.OnAnonymousSignInClicked -= OnAnonymousSignInClicked;
+		_loginPanel.OnEmailSignInClicked -= OnEmailSignInClicked;
+		_loginPanel.OnEmailSignUpClicked -= OnEmailSignUpClicked;
+		
+		_loginPanel.gameObject.SetActive(false);
+	}
+	
 
 	private void QuitGame() {
 #if UNITY_EDITOR
