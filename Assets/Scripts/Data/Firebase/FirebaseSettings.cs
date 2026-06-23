5e50da66d3ad57c707ef9302e24051c2251bc560
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-06-23 15:34:05 +0900

    refactor(#255) : Firebase 설정 enum 분리
    Firebase 인증 흐름과 초기화 상태를 여러 컴포넌트가 공유할 수 있도록 공용 enum으로 분리합니다.

--- Assets/Scripts/Data/Firebase/FirebaseSettings.cs
+++ Assets/Scripts/Data/Firebase/FirebaseSettings.cs
@@ -2,11 +2,6 @@
 
 [CreateAssetMenu(fileName = "FirebaseSettings", menuName = "Firebase/Settings")]
 public class FirebaseSettings : ScriptableObject {
-	public enum VerificationType {
-		Anonymous,
-		Email
-	}
-	
 	public bool UseFirebase;
 	public VerificationType Type;
 }
